b19278db54cc987699fea9ef9022ec83395b15dd
Dinesh Kumar <dinesh.kumar@apptunix.com>
2021-10-15 10:30:49 +0530

    google + and profile

--- app/src/androidTest/java/com/sirapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sirapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sir", appContext.getPackageName());
+        assertEquals("com.sirapp", appContext.getPackageName());
     }
 }
