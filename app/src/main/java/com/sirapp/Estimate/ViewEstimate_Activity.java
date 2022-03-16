d53be8a8ed5a735db925e33a6fa27586b69fb329
Dinesh Kumar <dinesh.kumar@apptunix.com>
2022-03-16 16:41:44 +0530

    first commit

--- app/src/main/java/com/sirapp/Estimate/ViewEstimate_Activity.java
+++ app/src/main/java/com/sirapp/Estimate/ViewEstimate_Activity.java
@@ -850,6 +850,7 @@ public class ViewEstimate_Activity extends BaseActivity {
                     .replaceAll("Estimate Date", htmlview_CreditNoteDate)
 //                        .replaceAll("Reference No", htmlview_ReferenceNo)
                     .replaceAll("SUMMARY", htmlview_SUMMARY)
+
                     .replaceAll("Product/Item", htmlview_ProductItem)
                     .replaceAll("Unit of Measurement", htmlview_UnitofMeasurement)
                     .replaceAll("Quantity", htmlview_Quantity)
