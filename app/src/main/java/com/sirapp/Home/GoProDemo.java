b532cc8fb3668a07a69ec3de11da1aba2c2f95df
Dinesh Kumar <dinesh.kumar@apptunix.com>
2022-04-25 18:58:16 +0530

    first commit

--- app/src/main/java/com/sirapp/Home/GoProDemo.java
+++ app/src/main/java/com/sirapp/Home/GoProDemo.java
@@ -1,4 +1,18 @@
 package com.sirapp.Home;
 
-public class GoProDemo {
+import android.os.Bundle;
+
+import com.sirapp.Base.BaseActivity;
+import com.sirapp.R;
+
+public class GoProDemo extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_pro);
+
+
+
+    }
 }
